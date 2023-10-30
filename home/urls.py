3d90ba7c50a82c54d5144192036356bc0425f278
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2023-10-30 07:38:11 +0530

    changing urls

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('kathan', views.index, name='index'),
+    path('', views.index, name='index'),
     path('scaler/translate', views.translate)
 ]
