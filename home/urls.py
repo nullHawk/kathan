78036a029c8447bb43920af93b1bc6b573fb7e24
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2023-10-30 06:41:34 +0530

    chnaged the url route of kathan UI
    changed url address of UI from root to "/kathan"

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('kathan', views.index, name='index'),
     path('scaler/translate', views.translate)
 ]
