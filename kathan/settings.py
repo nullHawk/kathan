a808d86a6ba5c082020da277907e058eb99da23a
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2023-10-31 10:30:01 +0530

    .

--- kathan/settings.py
+++ kathan/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'kathan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',  # Use the appropriate hostname or IP address
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
